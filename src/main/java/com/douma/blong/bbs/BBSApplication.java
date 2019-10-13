e0c6dc83c13ce43b1c486016c65e3444a5cc98cd
douma.hao <809103454@qq.com>
2019-10-13 21:12:41 +0800

    应用新建-springboot

---
+++ src/main/java/com/douma/blong/bbs/BBSApplication.java
@@ -0,0 +1,12 @@
+package com.douma.blong.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BBSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BBSApplication.class, args);
+    }
+}
